78898ea0556979aabb61ec2fbcb9e840c4f4efd5
mjahles <ahlmatj@dunwoody.edu>
2020-03-24 14:35:38 -0500

    correctly added in the method for step 2 and removed the incorrect method for step 3. Also initialized the variable int userInput for step 3.

--- Program.cs
+++ Program.cs
@@ -6,13 +6,25 @@ namespace modularization_pp_activity1
     {
         static void Main(string[] args)
         {
+            //Step 1
             //Declaring Global Variables
             string[] groceryListItems = {"Nuts", "Fish", "Lentils", "Whole Grains", "Beans", "Olive Oil", "Eggs", "Yogurt"};
             
-            public string userItem()
-            {
-                
-            }
+            int userInput;
+            outputGroceryList(groceryListItems);
+        }
+        
+        //Step 2
+        //Defining a method that outputs each of the items.
+        private static void outputGroceryList(string [] groceryList){
+            //We use groceryList here because it helps us see that we are talking about the array as an argument and not as the variable defined above.
+
+            //Output the groceryList
+            for(int x = 0; x < groceryList.Length; x++){
+                //Easy for loop. The x++ is the iteration. The x < groceryListItems is for determing the max number of cycles by the loop.
+            
+                Console.WriteLine(groceryList[x]);
+            }  
         }
     }
 }
