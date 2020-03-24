73607022ba6a35e0078269489e8bbd8464f97311
mjahles <ahlmatj@dunwoody.edu>
2020-03-24 15:20:59 -0500

    Added in the extra step activity that takes the user input and tells them what the item is as well as the price of that item

--- Program.cs
+++ Program.cs
@@ -10,15 +10,19 @@ namespace modularization_pp_activity1
             //Declaring Global Variables
             string[] groceryListItems = {"Nuts", "Fish", "Lentils", "Whole Grains", "Beans", "Olive Oil", "Eggs", "Yogurt"};
             
+            double [] groceryListItemsPrice = {.75, 3.00, 2.00, 1.00, 1.75, 7.49, 4.00, 1.29};
+            
             int userInput;
+            
             outputGroceryList(groceryListItems);
+            userInput = endUserSelection();
+            outputPrice(userInput, groceryListItems, groceryListItemsPrice);
 
-            endUserSelection();
         }
         
         //Step 2
         //Defining a method that outputs each of the items.
-        private static void outputGroceryList(string [] groceryList){ //This string [] statement within the method is stating that the argument that we are passing into it has to be an array and that it will be referred to as groceryList.
+        public static void outputGroceryList(string [] groceryList){ //This string [] statement within the method is stating that the argument that we are passing into it has to be an array and that it will be referred to as groceryList.
             //We use groceryList here because it helps us see that we are talking about the array as an argument and not as the variable defined above.
 
             //Output the groceryList
@@ -33,8 +37,9 @@ namespace modularization_pp_activity1
 
         //Step 3
         //Defining a method that asks the user to input their item.
-        public static void endUserSelection(){
+        public static int endUserSelection(){
 
+            //Local variable
             int userInput;
 
             Console.WriteLine("Please input a grocery list item");
@@ -42,6 +47,13 @@ namespace modularization_pp_activity1
             userInput = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"You entered item number {userInput}");
+
+            return userInput;
+        }
+
+        //Extra Step
+        public static void outputPrice(int userInput, string [] names, double [] price){
+            Console.WriteLine($"You entered item {userInput} {names[userInput]} for a price of {price[userInput].ToString("c")}");
         }
     }
 }
