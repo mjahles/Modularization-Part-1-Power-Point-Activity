8bcd9b38b75e3720ea48b96855fb5125d3899c97
mjahles <ahlmatj@dunwoody.edu>
2020-03-24 14:53:25 -0500

    Added in more notes and the basis for the method described in step 3

--- Program.cs
+++ Program.cs
@@ -12,19 +12,36 @@ namespace modularization_pp_activity1
             
             int userInput;
             outputGroceryList(groceryListItems);
+
+            endUserSelection();
         }
         
         //Step 2
         //Defining a method that outputs each of the items.
-        private static void outputGroceryList(string [] groceryList){
+        private static void outputGroceryList(string [] groceryList){ //This string [] statement within the method is stating that the argument that we are passing into it has to be an array and that it will be referred to as groceryList.
             //We use groceryList here because it helps us see that we are talking about the array as an argument and not as the variable defined above.
 
             //Output the groceryList
             for(int x = 0; x < groceryList.Length; x++){
                 //Easy for loop. The x++ is the iteration. The x < groceryListItems is for determing the max number of cycles by the loop.
             
-                Console.WriteLine(groceryList[x]);
+                Console.WriteLine($"Enter {x} for {groceryList[x]}");
+                        //The $"Enter {x} for part is string concatentation. What that is doing is modifying the output by adding in those statements.
+                        //The {x} becomes the number corresponding to the position of the item in the array. For example, "Nuts" are in position 0.
             }  
+        }
+
+        //Step 3
+        //Defining a method that asks the user to input their item.
+        public static void endUserSelection(){
+
+            int userInput;
+
+            Console.WriteLine("Please input a grocery list item");
+
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"You entered item number {userInput}");
         }
     }
 }
